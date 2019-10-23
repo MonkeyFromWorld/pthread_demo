dfc8002b82de15099ad82d13a4e564cb61d12bd1
NamedCXJ <18666632992@163.com>
2019-10-23 18:06:02 +0800

    1805

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <pthread.h>
+#include <windows.h>
+#include <unistd.h>
 using namespace std;
 #define NUM_THREADS 10
 #define EASY 0
-#define MID 1
+#define MID 0
+#define JOIN_DETACH 1
 #define DIFF 0
 
 void* sayHello() {
@@ -14,9 +17,18 @@ void* sayHello() {
 void *PrintHello(void *threadid) {
     // 对传入的参数进行强制类型转换，由无类型指针变为整形数指针，然后再读取
     int tid = *((int*)threadid);
-    cout << "Hello Runoob! 线程 ID: " << tid << endl;
+    cout << "Hello Runoob! 线程 ID: " << tid << "\n";
     pthread_exit(nullptr);
     return nullptr;
+}
+
+void *wait(void *t){
+    long tid = *(long*)t;
+
+    sleep(1);
+    cout << "Sleeping in thread " << endl;
+    cout << "Thread with id : " << tid << "  ...exiting " << endl;
+    pthread_exit(nullptr);
 }
 
 int main() {
@@ -36,16 +48,49 @@ int main() {
     int index[NUM_THREADS];
     int rc, i;
     for(i=0; i<NUM_THREADS; i++) {
-        cout << "main(),创建线程: " << i << endl;
+        cout << "main(),创建线程: " << i << "\n";
         index[i] = i;
         // 传入的时候必须强制转换为void* 类型，即无类型指针
-        rc = pthread_create(&threads[i], nullptr, PrintHello, (void *)&(index[i]));
+        rc = pthread_create(&threads[i], nullptr, reinterpret_cast<void *(*)(void *)>(PrintHello), (void *)&(index[i]));
         if(rc) {
             cout << "Error:无法创建线程," << rc << endl;
             exit(-1);
         }
     }
     pthread_exit(nullptr);
+#endif
+#if JOIN_DETACH
+    int rc;
+    int i;
+    pthread_t threads[NUM_THREADS];
+    pthread_attr_t attr;
+    void *status;
+
+    // 初始化并设置线程为可连接的（joinable）
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    for( i=0; i < NUM_THREADS; i++ ){
+        cout << "main() : creating thread, " << i << endl;
+        rc = pthread_create(&threads[i], NULL, wait, (void *)&i );
+        if (rc){
+            cout << "Error:unable to create thread," << rc << endl;
+            exit(-1);
+        }
+    }
+    // 删除属性，并等待其他线程
+    pthread_attr_destroy(&attr);
+    for( i=0; i < NUM_THREADS; i++ ){
+        rc = pthread_join(threads[i], &status);
+        if (rc){
+            cout << "Error:unable to join," << rc << endl;
+            exit(-1);
+        }
+        cout << "Main: completed thread id :" << i ;
+        cout << "  exiting with status :" << status << endl;
+    }
+
+    cout << "Main: program exiting." << endl;
+    pthread_exit(NULL);
 #endif
     return 0;
 }
