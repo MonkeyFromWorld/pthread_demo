7bec922d22fb07513600d299ccf0a51984bf0091
NamedCXJ <18666632992@163.com>
2019-10-24 09:58:31 +0800

    2011

--- main.cpp
+++ main.cpp
@@ -1,149 +1,11 @@
-#include <iostream>
-#include <pthread.h>
-#include <windows.h>
-#include <unistd.h>
-#include <thread>
-#include <backward/strstream>
+#include "thread_fun.h"
 
-using namespace std;
-#define NUM_THREADS 10
-#define NAME_LINE   40
-#define EASY 1
+#define EASY 0
 #define MID 0
+#define MID2 1
 #define JOIN_DETACH 0
 #define DIFF 0
-
-void* sayHello() {
-    cout << "Hello Robot!" << endl;
-    return nullptr;
-}
-
-void *PrintHello(void *threadid) {
-    // 对传入的参数进行强制类型转换，由无类型指针变为整形数指针，然后再读取
-    int tid = *((int*)threadid);
-    cout << "Hello Runoob! 线程 ID: " << tid << "\n";
-    pthread_exit(nullptr);
-    return nullptr;
-}
-
-void *wait(void *t){
-    long tid = *(long*)t;
-
-    sleep(1);
-    cout << "Sleeping in thread " << endl;
-    cout << "Thread with id : " << tid << "  ...exiting " << endl;
-    pthread_exit(nullptr);
-}
-
-DWORD WINAPI ThreadProc(LPVOID lpParameter){
-    for (int i = 0; i < 5; ++ i)
-    {
-        cout << "子线程:i = " << i << endl;
-        Sleep(100);
-    }
-    return 0L;
-}
-
-//定义线程函数传入参数的结构体
-typedef struct __THREAD_DATA
-{
-    int nMaxNum;
-    char strThreadName[NAME_LINE];
-
-    __THREAD_DATA() : nMaxNum(0)
-    {
-        memset(strThreadName, 0, NAME_LINE * sizeof(char));
-    }
-}THREAD_DATA;
-
-//线程函数
-DWORD WINAPI ThreadProc2(LPVOID lpParameter)
-{
-    auto* pThreadData = (THREAD_DATA*)lpParameter;
-
-    for (int i = 0; i < pThreadData->nMaxNum; ++ i)
-    {
-        cout << pThreadData->strThreadName << " --- " << i << endl;
-        //Sleep(50);
-    }
-    return 0L;
-}
-
-HANDLE g_hMutex = NULL;     //互斥量
-//线程函数
-DWORD WINAPI ThreadProc3(LPVOID lpParameter)
-{
-    THREAD_DATA* pThreadData = (THREAD_DATA*)lpParameter;
-
-    for (int i = 0; i < pThreadData->nMaxNum; ++ i)
-    {
-        //请求获得一个互斥量锁
-        WaitForSingleObject(g_hMutex, INFINITE);
-        cout << pThreadData->strThreadName << " --- " << i << endl;
-        Sleep(50);
-        //释放互斥量锁
-        ReleaseMutex(g_hMutex);
-    }
-    return 0L;
-}
-
-//定义线程函数传入参数的结构体
-typedef struct __TICKET
-{
-    int nCount;
-    char strTicketName[NAME_LINE];
-
-    __TICKET() : nCount(0)
-    {
-        memset(strTicketName, 0, NAME_LINE * sizeof(char));
-    }
-}TICKET;
-
-typedef struct __THD_DATA
-{
-    TICKET* pTicket;
-    char strThreadName[NAME_LINE];
-
-    __THD_DATA() : pTicket(NULL)
-    {
-        memset(strThreadName, 0, NAME_LINE * sizeof(char));
-    }
-}THD_DATA;
-
-//基本类型数据转换成字符串
-template<class T>
-string convertToString(const T val)
-{
-    string s;
-    strstream ss;
-    ss << val;
-    ss >> s;
-    return s;
-}
-
-//售票程序
-DWORD WINAPI SaleTicket(LPVOID lpParameter) {
-
-    THD_DATA *pThreadData = (THD_DATA *) lpParameter;
-    TICKET *pSaleData = pThreadData->pTicket;
-    while (pSaleData->nCount > 0) {
-        //请求获得一个互斥量锁
-        WaitForSingleObject(g_hMutex, INFINITE);
-        if (pSaleData->nCount > 0) {
-            cout << pThreadData->strThreadName << "出售第" << pSaleData->nCount-- << "的票,";
-            if (pSaleData->nCount >= 0) {
-                cout << "出票成功!剩余" << pSaleData->nCount << "张票." << endl;
-            } else {
-                cout << "出票失败！该票已售完。" << endl;
-            }
-        }
-        //Sleep(1);
-        //释放互斥量锁
-        ReleaseMutex(g_hMutex);
-    }
-
-    return 0L;
-}
+extern HANDLE g_hMutex;//互斥量
 
 int main() {
 #if EASY
@@ -171,6 +33,37 @@ int main() {
             exit(-1);
         }
     }
+    pthread_exit(nullptr);
+#endif
+#ifdef MID2
+    // 定义线程的 id 变量，多个变量使用数组
+    /*
+    pthread_t tids[NUM_THREADS];
+    for(int i=0; i<NUM_THREADS; i++) {
+        //参数依次是：创建的线程id，线程参数，调用的函数，传入的函数参数
+        int ret = pthread_create(&tids[i], nullptr, reinterpret_cast<void *(*)(void *)>(sayHello), nullptr);
+        if(ret != 0)
+            cout << "pthread_create error: error_code=" << ret << endl;
+    }
+     */
+    g_hMutex = CreateMutex(NULL, FALSE, NULL);
+    //创建第一个子线程
+    HANDLE hThread1 = CreateThread(NULL, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(sayHello), NULL, 0, NULL);
+    //创建第二个子线程
+    HANDLE hThread2 = CreateThread(NULL, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(sayHello), NULL, 0, NULL);
+    //关闭线程
+    CloseHandle(hThread1);
+    CloseHandle(hThread2);
+    for (int i = 0; i < NUM_THREADS; ++ i)
+    {
+        //请求获得一个互斥量锁
+        WaitForSingleObject(g_hMutex, INFINITE);
+        cout << "主线程 === " << i << endl;
+        Sleep(50);
+        //释放互斥量锁
+        ReleaseMutex(g_hMutex);
+    }
+    system("pause");
     pthread_exit(nullptr);
 #endif
 #if JOIN_DETACH
@@ -229,7 +122,7 @@ int main() {
     threadData2.nMaxNum = 10;
     strcpy(threadData2.strThreadName, "线程2");
     //创建第一个子线程
-    HANDLE hThread1 = CreateThread(NULL, 0, ThreadProc2, &threadData1, 0, NULL);
+    HANDLE hThread1 = CreateThread(NULL, 0, ThreadProc2, &threadData2, 0, NULL);
     //创建第二个子线程
     HANDLE hThread2 = CreateThread(NULL, 0, ThreadProc2, &threadData2, 0, NULL);
     //关闭线程
